8966771339b69d733f22c3abcbf2cd0a0e7682fb
rakis <rakis_me@protonmail.com>
2022-05-14 15:56:36 +0000

    stop point arweave - querying gql

--- src/lib/api.js
+++ src/lib/api.js
@@ -21,7 +21,40 @@ export const createPostInfo = (node) => {
 }
 
 export const buildQuery = () => {
-  const queryObject = {}
+  const queryObject = {
+    query: `
+query {
+  transactions(
+    first: 100,
+    tags: [
+      { name: "App-Name", values: ["PublicSquare"]},
+      { name: "Content-Type", values: ["text/plain"]}
+    ]
+  ) {
+    edges {
+      node {
+        id
+        owner {
+          address
+        }
+        data {
+          size
+        }
+        block {
+          height
+          timestamp
+        }
+        tags {
+          name
+          value
+        }
+      }
+    }
+  }
+} 
+    
+    `
+  }
   return queryObject;
 }
 
