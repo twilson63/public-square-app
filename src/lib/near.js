21b93d64f02a5300297150e877516672b87d874c
Tom Wilson <tom@hyper.io>
2022-05-16 10:46:49 -0400

    Update near.js

--- src/lib/near.js
+++ src/lib/near.js
@@ -4,35 +4,26 @@ import { WalletConnection } from "near-api-js";
 const { connect, keyStores } = nearAPI;
 
 const NEAR_OPTS = {
-  networkId: "mainnet",
-  keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-  nodeUrl: "https://rpc.mainnet.near.org",
-  walletUrl: "https://wallet.mainnet.near.org",
-  helperUrl: "https://helper.mainnet.near.org",
+
 };
 
 
 export const getWallet = async () => {
-  const near = await connect(NEAR_OPTS);
-  return new WalletConnection(near, "bundlr");
+  return null
 }
 
 export const isSignedIn = async () => {
-  const wallet = await getWallet()
-  return wallet.isSignedIn()
+  return false
 }
 
 export const getAccountId = async () => {
-  const wallet = await getWallet()
-  return wallet.getAccountId()
+  return null
 }
 
 export const signIn = async () => {
-  const wallet = await getWallet()
-  wallet.requestSignIn()
+  return null
 }
 
 export const signOut = async () => {
-  const wallet = await getWallet()
-  wallet.signOut()
+  return null
 }
